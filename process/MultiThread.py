53daee331b0cd055260439d0f12319f6198e7ecb
徐盘根 <xpg@alphathink.org>
2018-07-25 17:58:25 +0800

    * 1.多线程实例修改

--- process/MultiThread.py
+++ process/MultiThread.py
@@ -4,12 +4,17 @@
 __author__ = 'click'
 __date__ = '2018/7/23 下午6:20'
 
-import time, threading, myio.ReadAndWrite
+import time, threading
 
 
 def threadReadFile():
     print('主线程的名字%s' % threading.current_thread().name)
-    print('读取文件中内容是%s' % myio.ReadAndWrite.readTxt())
+    # print('读取文件中内容是%s' % myio.ReadAndWrite.readTxt())
+    n = 0
+    while n < 10:
+        n = n + 1
+
+    print('执行相加操作结果是%s' % n)
 
 
 t = threading.Thread(target=threadReadFile, name='threadReadFile')
